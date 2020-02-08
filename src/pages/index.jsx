3b7f70d5e48d59e574307d0410561a0646f24a76
Anson <git@lichtf.us>
2020-02-08 15:29:40 -0700

    Remove old sections

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,11 @@
-import { motion, useTransform, useViewportScroll } from 'framer-motion';
 import { graphql } from 'gatsby';
-import React, { useLayoutEffect, useRef, useState } from 'react';
-import Arrow from '../components/assets/Arrow';
+import React from 'react';
 import Plus from '../components/assets/plus';
 import Ticker from '../components/assets/ticker';
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 
 const contentWidth = 'mx-auto max-w-5xl';
-
-const ParallaxImage = ({ src, ...style }) => {
-  const [elementTop, setElementTop] = useState(0);
-  const ref = useRef(null);
-  const { scrollY } = useViewportScroll();
-
-  const y = useTransform(scrollY, [elementTop, elementTop + 3], [0, -1], {
-    clamp: false,
-  });
-
-  useLayoutEffect(() => {
-    const element = ref.current;
-    setElementTop(element.offsetTop);
-  }, [ref]);
-
-  return (
-    <div ref={ref} className="image-container absolute" style={{ zIndex: -1 }}>
-      <motion.div className="overlay" style={{ ...style, y, zIndex: -1 }}><img className="object-cover w-full" src={src} alt="" /></motion.div>
-    </div>
-  );
-};
 
 const BlogIndex = ({ data, location }) => {
   const { site, allMarkdownRemark } = data;
@@ -37,7 +14,10 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <div>
-      <div className="absolute top-0 opacity-75 -mt-24 w-screen flex justify-center" style={{ zIndex: -1 }}>
+      <div
+        className="absolute top-0 opacity-75 -mt-24 w-screen flex justify-center"
+        style={{ zIndex: -1 }}
+      >
         <img src="homepageprofile.jpg" />
       </div>
       {/* <img className="section-2-image absolute top-0 right-0 pointer-events-none" src="foreground-grenery-1.png" /> */}
@@ -51,130 +31,23 @@ const BlogIndex = ({ data, location }) => {
             </div>
           </div>
           <div className="front-layer absolute top-0 right-0 flex flex-col items-end mt-24 w-full">
-            <div className="font-black font-display text-display-title uppercase leading-none">Designer</div>
-            <div className="font-black font-display text-display-title uppercase leading-none">Developer</div>
-            <Ticker textItems={['ReactJS', 'Accessibility', 'Javascript', 'Graphic Design', 'GatsbyJS', 'Object Oriented Programming', 'Functional Programming']} />
-          </div>
-        </div>
-
-        <div className="section-divider flex items-center justify-center opacity-25 mt-24 mb-24 pt-24 pb-24"><div className="w-8"><Arrow /></div></div>
-        <div className={`${contentWidth} section-2 relative`}>
-          <div className="text-section text-lg">
-            <p>That small little animation as an application loads?</p>
-            <p className="font-display uppercase font-black text-2xl ml-12">It matters.</p>
-            <p>A well-written error response to a poorly formed email address?</p>
-            <p className="font-display uppercase font-black text-2xl ml-12">That matters.</p>
-            <p>
-              Responsive UI that works on any screen, without sacrificing critical functionality?
-            </p>
-            <p className="font-display uppercase font-black text-2xl ml-12">Also matters.</p>
-            <p className="text-center mt-24">because</p>
-            <div className="flex flex-col items-center justify-center mx-auto font-black font-display text-display-title uppercase leading-none">
-              <div>
-
-                <ParallaxImage
-                  src="foreground-grenery-1.png"
-                  width="100vw"
-                  height="100vh"
-                  right={0}
-                  top={200}
-                />
-                <div>Details</div>
-                <div>matter.</div>
-              </div>
+            <div className="font-black font-display text-display-title uppercase leading-none">
+              Designer
             </div>
-          </div>
-        </div>
-
-
-        <ParallaxImage
-          src="foreground-grenery-2.png"
-          width="100vw"
-          height="100vh"
-          left={0}
-          top={200}
-        />
-        <div className="section-divider flex items-center justify-center opacity-25 mt-24 mb-24 pt-24 pb-24"><div className="w-8"><Arrow /></div></div>
-        <div className={`${contentWidth} section-3`}>
-          <div className="text-section text-right text-lg">
-            <p>The small-town client with big-world dreams.</p>
-            <p className="font-display uppercase font-black text-2xl ml-12">She matters.</p>
-            <p>A user with color-blindness, using the product to run a business.</p>
-            <p className="font-display uppercase font-black text-2xl ml-12">He matters.</p>
-            <p>
-              Developers, itching to build their bright new idea using your API.
-            </p>
-            <p className="font-display uppercase font-black text-2xl ml-12">They matter.</p>
-            <p className="text-center mt-24">because</p>
-            <div className="flex flex-col items-center justify-center text-left mx-auto font-black font-display text-display-title uppercase leading-none">
-              <div>
-                <div>People</div>
-                <div>matter.</div>
-              </div>
+            <div className="font-black font-display text-display-title uppercase leading-none">
+              Developer
             </div>
-          </div>
-        </div>
-        <div className="section-divider flex items-center justify-center opacity-25 mt-24 mb-24 pt-24 pb-24"><div className="w-8"><Arrow /></div></div>
-        <ParallaxImage
-          src="major-green-background.png"
-          width="100vw"
-          height="100vh"
-          left={0}
-          top={0}
-        />
-        <div className="section-divider text-center pb-24 text-2xl">All of this is only possible because...</div>
-
-
-        <div className={`${contentWidth} section-4 h-screen pt-48`}>
-          <div className="max-w-5xl flex flex-col text-left text-white mx-auto font-black font-display text-display-title uppercase leading-none">
-            <div>
-              <div>You</div>
-              <div>matter.</div>
-            </div>
-          </div>
-        </div>
-
-        <ParallaxImage
-          src="major-green-background.png"
-          width="100vw"
-          height="100vh"
-          left={0}
-          top={200}
-        />
-        <div className="spaceholder w-screen h-screen" />
-
-        <div className={`${contentWidth} section-5 pb-48`}>
-          <div className="max-w-5xl flex flex-col text-left text-white mx-auto font-semibold font-display text-6xl leading-none">
-            <div>
-              <div>let's dive in.</div>
-              <div className="rounded-lg bg-white text-black p-8 font-light flex mb-4">
-                <div className="button-ticker-container overflow-hidden flex-1 relative">
-                  <Ticker textItems={['Send a message']} />
-                  <div className="button-ticker-cover absolute top-0 left-0 w-full h-full" style={{ boxShadow: '-150px 0 50px -50px #fff inset' }} />
-                </div>
-                <div className="w-8"><Arrow /></div>
-              </div>
-              <div className="flex -mx-2">
-                <div className="px-2 w-1/2">
-                  <div className="rounded-lg bg-white text-black p-8 font-light flex">
-                    <div className="button-ticker-container overflow-hidden flex-1 relative">
-                      <Ticker textItems={['LinkedIn']} />
-                      <div className="button-ticker-cover absolute top-0 left-0 w-full h-full" style={{ boxShadow: '-150px 0 50px -50px #fff inset' }} />
-                    </div>
-                    <div className="w-8"><Arrow /></div>
-                  </div>
-                </div>
-                <div className="px-2 w-1/2">
-                  <div className="rounded-lg bg-white text-black p-8 font-light flex">
-                    <div className="button-ticker-container overflow-hidden flex-1 relative">
-                      <Ticker textItems={['GitHub']} />
-                      <div className="button-ticker-cover absolute top-0 left-0 w-full h-full" style={{ boxShadow: '-150px 0 50px -50px #fff inset' }} />
-                    </div>
-                    <div className="w-8"><Arrow /></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Ticker
+              textItems={[
+                'ReactJS',
+                'Accessibility',
+                'Javascript',
+                'Graphic Design',
+                'GatsbyJS',
+                'Object Oriented Programming',
+                'Functional Programming'
+              ]}
+            />
           </div>
         </div>
       </Layout>
