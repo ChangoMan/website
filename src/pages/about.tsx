d32a95515883dc5917a979de2628a0afa53ba720
Anson <git@lichtf.us>
2020-02-29 17:35:40 -0700

    feat(about): built layout for About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,73 +6,41 @@ const AboutIndex = () => (
   <Layout>
     <SEO title="About" />
     <div className="max-w-2xl my-0 mx-auto">
-      <h2 className="font-black font-display text-6xl">About</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-        euismod eros non consequat faucibus. Donec in dictum nunc. Quisque a leo
-        ipsum. Donec elit lorem, mattis id congue vitae, bibendum id eros. Duis
-        volutpat rutrum lectus id luctus. In hac habitasse platea dictumst.
-        Aenean interdum massa varius dui tempus, sed ornare velit porta. Vivamus
-        et finibus mauris. Nam porta augue a ipsum imperdiet fringilla. Aenean
-        sit amet sodales augue. Phasellus sit amet ipsum convallis, finibus
-        turpis a, mollis leo. Quisque nec massa velit. Vestibulum fermentum
-        metus scelerisque nisl egestas convallis. Nulla facilisi. Class aptent
-        taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-        himenaeos. Pellentesque elit orci, elementum ut ullamcorper sed,
-        accumsan quis tellus.
+      <div className="text-center mb-8">
+        <h2 className="font-black font-display text-xl inline-block border-solid border-b-4 border-gray-400 mb-5">
+          ABOUT
+        </h2>
+      </div>
+
+      <div
+        className="float-right mb-5 px-8 ml-5 overflow-hidden relative"
+        style={{ maxWidth: '50%', boxShadow: '0 -1px 0 rgba(0,0,0,0.3) inset' }}
+      >
+        <img
+          className="relative"
+          style={{ zIndex: -1 }}
+          src="about-profile-photo.jpg"
+        />
+      </div>
+      <p className="text-3xl font-display tracking-tight">
+        Hey there, I'm Anson.
+      </p>
+      <p className="text-xl mb-5">Developer by trade, designer by passion.</p>
+      <p className="mb-4">
+        Quisque a leo ipsum. Donec elit lorem, mattis id congue vitae, bibendum
+        id eros. Duis volutpat rutrum lectus id luctus. In hac habitasse platea
+        dictumst. Aenean interdum massa varius dui tempus, sed ornare velit
+        porta. Vivamus et finibus mauris. Nam porta augue a ipsum imperdiet
+        fringilla. Aenean sit amet sodales augue. Phasellus sit amet ipsum
+        convallis, finibus turpis a, mollis leo. Quisque nec massa velit.
+        Vestibulum fermentum metus scelerisque nisl egestas convallis. Nulla
+        facilisi. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Pellentesque elit orci, elementum ut
+        ullamcorper sed, accumsan quis tellus.
       </p>
       <p>
-        Curabitur fermentum est mollis nunc pellentesque ultricies. Aenean quis
-        quam imperdiet, blandit risus a, interdum sem. Etiam iaculis iaculis
-        odio, sed faucibus nisl pharetra id. Quisque sit amet risus vitae odio
-        dictum rhoncus. Ut faucibus justo et auctor rutrum. Morbi luctus justo
-        sit amet elit bibendum lacinia. Nunc auctor semper consequat. Praesent
-        gravida vestibulum massa, id bibendum purus cursus eget. Nulla nec diam
-        leo. Pellentesque sapien purus, varius vel ante et, semper vestibulum
-        ligula. Nulla porttitor egestas sapien, ac tristique tortor gravida ac.
-        Pellentesque volutpat augue id nisi mattis, et porta odio fermentum.
-        Fusce at erat ultricies, convallis ipsum dictum, pretium eros.
-      </p>
-      <p>
-        Fusce rutrum, nisi ut volutpat cursus, elit nisi auctor mi, sed maximus
-        odio libero non urna. Fusce at efficitur augue. Donec eu pellentesque
-        libero, facilisis tincidunt libero. Integer nibh justo, tincidunt a
-        vestibulum in, sodales eget nisl. Etiam porttitor gravida risus, sed
-        volutpat orci iaculis id. Aenean ac orci condimentum, venenatis leo at,
-        consequat tellus. Vivamus cursus sapien quam, nec iaculis nunc pretium
-        in. Donec ac maximus diam. Aenean quis arcu blandit, tempus leo in,
-        tempor lacus. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Cras eget turpis ac diam
-        condimentum lacinia eget ac sem. Phasellus congue lorem in euismod
-        gravida. Nam suscipit dapibus consequat. Suspendisse convallis massa
-        eget scelerisque vehicula. Sed libero purus, maximus eu augue eu, auctor
-        placerat nulla.
-      </p>
-      <p>
-        Pellentesque ut velit in leo tristique ullamcorper at et est. Vestibulum
-        ultrices, turpis cursus feugiat dictum, erat lacus porta nulla, ut
-        auctor est magna non turpis. Pellentesque dictum eu mauris quis aliquet.
-        Mauris porta est in suscipit ultricies. Donec imperdiet, risus at
-        imperdiet ullamcorper, odio nisl tempus quam, ac efficitur enim orci non
-        sapien. Nunc non convallis lorem, vel vehicula urna. Nunc aliquam
-        sodales turpis. Aenean nec libero eu nisi suscipit pulvinar. Aliquam sed
-        tempus urna. Donec eu pharetra purus, vitae malesuada augue. Donec at
-        dignissim ligula. Vivamus ac leo sapien. Donec lobortis pulvinar lacus
-        eu porta. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Aenean non felis ac lacus commodo convallis.
-      </p>
-      <p>
-        Nulla sed lorem nisl. Ut ultrices odio urna, non lacinia erat iaculis
-        et. Aliquam accumsan neque ipsum, eget vestibulum nibh placerat luctus.
-        Aliquam et erat et lacus efficitur consequat. Sed bibendum quam nulla,
-        sit amet pharetra nisi mattis malesuada. Praesent et vulputate felis.
-        Aenean aliquet, urna efficitur eleifend dictum, dolor purus pharetra
-        nibh, vel placerat tortor nisi ac diam. Duis vehicula nisl eget dolor
-        sollicitudin, nec luctus lacus gravida. Pellentesque vel metus sed
-        libero luctus egestas. Sed et maximus felis, sed faucibus enim.
-        Pellentesque pulvinar ligula in eros iaculis, vitae vulputate massa
-        vehicula. Curabitur ut maximus metus. Integer sed massa id velit
-        ultricies volutpat a vitae ligula.
+        Connect with me on: <a href="#">LinkedIn</a>, <a href="#">Twitter</a>,
+        and <a href="#">Instagram</a>.
       </p>
     </div>
   </Layout>
